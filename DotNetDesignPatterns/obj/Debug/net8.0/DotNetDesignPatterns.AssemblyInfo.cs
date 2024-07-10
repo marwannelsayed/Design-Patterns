d5bf8a64d3e62e4c2b21a0cf329b30053c5d812c
marwannelsayed <marwanyehiaelsayed@gmail.com>
2024-07-10 12:37:53 +0300

    msh last brdo

--- DotNetDesignPatterns/obj/Debug/net8.0/DotNetDesignPatterns.AssemblyInfo.cs
+++ DotNetDesignPatterns/obj/Debug/net8.0/DotNetDesignPatterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDesignPatterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01943d7cc42d984006a2a00959a8576bd41f00c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDesignPatterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDesignPatterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
